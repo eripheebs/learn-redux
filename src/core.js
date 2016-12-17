7548267c5024313bf2181d8de28ad88955f4083d
eripheebs <erikapheby@gmail.com>
2016-12-17 13:25:50 +0000

    Immutable updateIn, changes vote tally

--- src/core.js
+++ src/core.js
@@ -11,3 +11,13 @@ export function next(state){
     entries: entries.skip(2)
   });
 }
+
+export function vote(state, entry){
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
+}
+
+//Using updateIn makes this pleasingly succinct. What the code expresses is "reach into the nested data structure path ['vote', 'tally', 'Trainspotting'], and apply this function there. If there are keys missing along the path, create new Maps in their place. If the value at the end is missing, initialize it with 0".
