b8a1999e77d6cad55e9b5f9d15b0d1655acfbc67
eripheebs <erikapheby@gmail.com>
2016-12-18 08:08:36 +0000

    puts winner back in

--- src/core.js
+++ src/core.js
@@ -4,14 +4,6 @@ export function setEntries(state, entries){
   return state.set('entries', List(entries));
 }
 
-export function next(state){
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
-}
-
 export function vote(state, entry){
   return state.updateIn(
     ['vote', 'tally', entry],
@@ -21,3 +13,22 @@ export function vote(state, entry){
 }
 
 //Using updateIn makes this pleasingly succinct. What the code expresses is "reach into the nested data structure path ['vote', 'tally', 'Trainspotting'], and apply this function there. If there are keys missing along the path, create new Maps in their place. If the value at the end is missing, initialize it with 0".
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
