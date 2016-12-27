bfcdaff41563332951f15a8dbd6a9d386d596aa9
eripheebs <erikapheby@gmail.com>
2016-12-27 05:26:35 +0000

    reducers, and using switch statement

---
+++ test/reducer_spec.js
@@ -0,0 +1,52 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+  it('handles SET_ENTRIES', function() {
+    const initialState = Map();
+    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['Trainspotting']
+    }));
+  });
+
+  it('handles NEXT', function() {
+    const initialState = fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE', function() {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      entries: []
+    }));
+  });
+
+});
